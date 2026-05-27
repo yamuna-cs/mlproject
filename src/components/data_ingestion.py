a9af17721bd9e7698748a16e205ec67f30047c69
yamuna-cs <ai.yamunavr@gmail.com>
2026-05-27 17:31:33 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,6 +4,11 @@ from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
@@ -44,4 +49,8 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data, test_data)
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_trainer(train_arr, test_arr)
---
+++ src/utils.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+from sklearn.metrics import r2_score
+import dill
+
+import numpy as np
+import pandas as pd
+
+from src.exception import CustomException
+
+def save_object(file_path: str, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_model(X, y, X_test, y_test, models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X, y)
+            
+            y_train_pred = model.predict(X)
+            train_model_score = r2_score(y, y_train_pred)
+            
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+            
+            report[list(models.keys())[i]] = test_model_score
+            
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
